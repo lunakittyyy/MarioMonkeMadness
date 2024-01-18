325522b7cfa04f4f5c542aa446a787321adee2d4
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-01-18 10:43:43 -0500

    update guid

--- MarioMonkeMadness/PluginInfo.cs
+++ MarioMonkeMadness/PluginInfo.cs
@@ -5,7 +5,7 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.luna.gorillatag.mariomonkemadness";
+        public const string GUID = "luna.mariomonkemadness";
         public const string Name = "MarioMonkeMadness";
         public const string Version = "1.0.0";
     }
